0f95f0eefe589694581f6228fe753d7ab9069d5b
Jake Carter <[80532104+D-Monkey@users.noreply.github.com]>
2024-08-21 19:55:31 +0300

    add computer choice funtcion to js page

--- rPSGame.js
+++ rPSGame.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice() {
+  let choice = Math.random();
+  if (0 <= choice < 1 / 3) {
+    console.log("Rock!");
+  } else if (1 / 3 <= choice < 2 / 3) {
+    console.log("Paper!");
+  } else if (2 / 3 <= choice <= 1) {
+    console.log("Scissors!");
+  }
+}
